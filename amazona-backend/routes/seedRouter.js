ab720ba8f25b693845ec28b59e8d3ded98e2350f
fakham <mohammed.fakham@gmail.com>
2023-03-24 18:45:15 +0000

    seed products to mongodb and use them

---
+++ amazona-backend/routes/seedRouter.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import data from '../data.js';
+import Product from '../models/product.js';
+
+const seedRouter = express.Router();
+
+seedRouter.get('/', async (req, res) => {
+  await Product.deleteMany({});
+  const createdProducts = await Product.insertMany(data.products);
+  res.send({ createdProducts });
+});
+
+export default seedRouter;
