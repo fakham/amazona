ebe2e1ce8758abe10576bf3d625fe0a2f9c6367b
fakham <mohammed.fakham@gmail.com>
2023-03-28 22:21:12 +0000

    create simple users

--- amazona-backend/data.js
+++ amazona-backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Admin User',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Customer User',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
