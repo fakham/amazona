6d2d1f2db491d81b1927bb9dc9a83d4f815297e6
fakham <mohammed.fakham@gmail.com>
2023-03-29 00:00:06 +0000

    sign in api

--- amazona-backend/server.js
+++ amazona-backend/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import data from './data.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRouter.js';
 import productRouter from './routes/productRouter.js';
+import userRouter from './routes/userRouter.js';
 
 dotenv.config();
 
@@ -17,8 +17,16 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 
